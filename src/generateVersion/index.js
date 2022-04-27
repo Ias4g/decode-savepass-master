768cc2096aef0b08d7d5bc81777a6e6949552ea2
Izael silva <62667424+Ias4g@users.noreply.github.com>
2022-04-27 18:37:10 -0300

    Updates

--- src/generateVersion/index.js
+++ src/generateVersion/index.js
@@ -1,14 +1,50 @@
-function generateVersion() {
-    const date = Date.now()
-    const myArrow = String(date)
+const date = Date.now()
+// const completeName = 'Kelly vitoria alves da silva'
 
-    const numberPrimary = myArrow.slice(0, 1)
-    const numberSecundary = myArrow.slice(1, 3)
-    const numberRest = myArrow.slice(3, myArrow.length)
+// function getSum(n) {
+//     let sum = 0;
+//     while (n != 0) {
+//         sum += n % 10;
+//         n = parseInt(n / 10);
+//     }
+//     return sum
+// }
+// const soma = getSum(date)
 
-    let versionApp = `${numberPrimary}.${numberSecundary}.${numberRest}`
 
-    console.log(versionApp)
+function generateVersion(date) {
+    const myArrowString = String(date)
+    const numberPrimary = myArrowString.slice(0, 1)
+    const numberSecundary = myArrowString.slice(1, 3)
+    const numberRest = myArrowString.slice(3, myArrowString.length)
+
+    const versionApp = `${numberPrimary}.${numberSecundary}.${numberRest}`
+
+    return versionApp
 }
+const version = generateVersion(date)
 
-generateVersion()
+
+// function nameSeparation(word) {
+//     let initial = ''
+//     const nSeparation = word.split(' ')
+
+//     for (let i = 0; i < nSeparation.length; i++) {
+//         initial += nSeparation[i][0]
+//     }
+
+//     return initial.toUpperCase()
+// }
+// const nome = nameSeparation(completeName)
+
+
+// function generatePassword(i, s) {
+//     return `${i}${s}`
+// }
+// const password = generatePassword(nome, soma)
+
+
+// console.log(`Soma: ${soma}`)
+console.log(`Versão: ${version}`)
+// console.log(`Password: ${password}`)
+// console.log(`Iniciais do nome: ${nome}`)
